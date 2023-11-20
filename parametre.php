921f4a2466e068395e9f04ca1bcd5ba15ab1f8c3
Charlotte GAERTNER <charlotte.gaertner@outlook.fr>
2023-11-20 12:33:00 +0100

    email address mandatory

--- parametre.php
+++ parametre.php
@@ -22,6 +22,13 @@ if (!empty($_POST)) {
                 <strong>Modification de login: </strong>Login non valide.
             </div>
             <?php
+        } else if ($email === '') {
+            ?>
+            <div class="alert alert-dismissible alert-danger">
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                <strong>Modification d'adresse email : </strong> L'adresse email est obligatoire.
+            </div>
+            <?php
         } else {
         $modifyUser =  modifyInfoUser($login, $firstname, $lastname, $birthdate, $email, $phone, $id); 
         $pdo->commit();
@@ -47,11 +54,11 @@ echo '<h2>Modifier vos informations "' . $thisUser[0]['login'] . '".</h2><br/<br
 <div>
     <form method="post">
         <table class="col-md-5">
-            <tr><th>Identifiant :</th><td><input class="form-control" type="text" name="login" value="<?php echo $thisUser[0]['login'];?>"></td></tr>
+            <tr><th>*Identifiant :</th><td><input class="form-control" type="text" name="login" value="<?php echo $thisUser[0]['login'];?>" required></td></tr>
             <tr><th>Prénom : </th><td><input class="form-control" type="text" name="firstname" value="<?php echo $thisUser[0]['firstname'];?>"/></td></tr>
             <tr><th>Nom : </th><td><input class="form-control" type="text" name="lastname" value="<?php echo $thisUser[0]['lastname']; ?>"/></td></tr>
             <tr><th>Date de naissance : </th><td><input class="form-control" type="date" name="birthdate" value="<?php echo $thisUser[0]['birthdate'];?>"/></td></tr>
-            <tr><th>Email : </th><td><input class="form-control" type="email" name="email" value="<?php echo $thisUser[0]['email'];?>"/></td></tr>
+            <tr><th>*Email : </th><td><input class="form-control" type="email" name="email" value="<?php echo $thisUser[0]['email'];?>"/ required></td></tr>
             <tr><th>téléphone : </th><td><input type="tel" class="form-control" name="phone" value="<?php echo $thisUser[0]['phone'];?>"/></td></tr>
             <tr><td><input type="submit" value="Enregistrer" class="btn btn-primary"></td></tr>
         </table>
